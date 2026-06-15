5da28b2c464709f1dd7b821d44b23fa2253872c3
knot <arjablc@gmail.com>
2026-06-15 18:44:21 +0545

    cmd file

--- lua/knot/core/autocmd.lua
+++ lua/knot/core/autocmd.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
-
+--
 -- no auto continue comments on new line
 vim.api.nvim_create_autocmd("FileType", {
 	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
